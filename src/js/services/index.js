40de5d23ef4a773d092cd003fd746fcef3a4a372
LAPTOP-1FCRCH5O\jpoli <jpolivadoti@loycus.com>
2017-04-20 15:41:17 +0200

    Displaying a profile page from server data 1

--- src/js/services/index.js
+++ src/js/services/index.js
@@ -5,8 +5,10 @@ let servicesModule = angular.module('app.services', []);
 
 import UserService from './user.service';
 import JwtService from './jwt.service';
+import ProfileService from './profile.service';
 
 export default servicesModule
   .service('User', UserService)
   .service('JWT', JwtService)
+  .service('Profile', ProfileService)
   .name;
