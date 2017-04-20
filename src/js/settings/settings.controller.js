e4770ef79e600d3b4ddf7df366e08b7ab2ce5187
LAPTOP-1FCRCH5O\jpoli <jpolivadoti@loycus.com>
2017-04-20 13:20:40 +0200

    Updating user settings 1

--- src/js/settings/settings.controller.js
+++ src/js/settings/settings.controller.js
@@ -1,9 +1,23 @@
 class SettingsCtrl {
-  constructor() {
+  constructor(User) {
     'ngInject';
 
+    // this._User = User;
 
+    this.formData = {
+      username: User.current.username,
+      image: User.current.image,
+      bio: User.current.bio,
+      email: User.current.email
+    };
+
+
+    this.logout = User.logout.bind(User);
   }
+
+  // logout(){
+  //   this._User.logout();
+  // }
 
 }
 
