25fb0f38fd93d53aae8a2dd014a7ebe5697d884f
LAPTOP-1FCRCH5O\jpoli <jpolivadoti@loycus.com>
2017-04-21 10:43:07 +0200

    Creating a follow button component with encapsulated functionality

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -4,12 +4,14 @@ let componentsModule = angular.module('app.components', []);
 
 //Component
 import ListErrors from './listErrors.component';
+import FollowBtn from './buttons/followBtn.component';
 
 //Directive
 import ShowAuthed from './showAuthed.directive';
 
 export default componentsModule
   .component('listErrors', ListErrors)
+  .component('followBtn', FollowBtn)
   //Directive
   .directive('showAuthed', ShowAuthed)
   .name;
