099282d3e41251e0895b68590c402499e2edf033
LAPTOP-1FCRCH5O\jpoli <jpolivadoti@loycus.com>
2017-04-27 13:34:03 +0200

    Displaying lists of articles

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -10,6 +10,8 @@ import ListErrors from './listErrors.component';
 import FollowBtn from './buttons/followBtn.component';
 import FavoriteBtn from './buttons/favoriteBtn.component';
 import ArticleMeta from './article-helpers/article-meta.component';
+import ArticlePreview from './article-helpers/article-preview.component';
+import ArticleList from './article-helpers/article-list.component';
 
 export default componentsModule
   //Directive
@@ -19,4 +21,6 @@ export default componentsModule
   .component('followBtn', FollowBtn)
   .component('favoriteBtn', FavoriteBtn)
   .component('articleMeta', ArticleMeta)
+  .component('articlePreview', ArticlePreview)
+  .component('articleList', ArticleList)
   .name;
